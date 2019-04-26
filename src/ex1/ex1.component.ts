cfe3f8cc7740d2bcbdbb4bc802ee5bf4bd4f0c50
jbranjith <onlineranjithsekar@gmail.com>
2019-04-26 15:02:53 +0530

    2 files modified, 1 added

--- src/ex1/ex1.component.ts
+++ src/ex1/ex1.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ex1',
-  templateUrl: './ex1.component.html'
+  templateUrl: './ex1.component.html',
+  styleUrls: ['./ex1.component.css']
 })
 export class Ex1Component {
   tooltipTitle = "Description";
   tooltipText = "A transmission is a machine in a transmission system, which provides controlled application of the power. A transmission is a machine in a transmission system, which provides controlled application of the power.";
-  data = "A transmission is a machine in a transmission system, which provides controlled application of the power..."
+  data = "A transmission is a machine in a transmission system, which provides controlled application of the power"
 }
